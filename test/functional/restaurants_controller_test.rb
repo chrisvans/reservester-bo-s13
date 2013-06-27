25da79d127031e7e288de47f62ce57d8bb4ee0d6
Tracy Moscato <tracy@mint>
2013-06-27 00:08:03 -0400

    Imagemaker and mysql

--- test/functional/restaurants_controller_test.rb
+++ test/functional/restaurants_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionController::TestCase
-  setup do
-    @restaurant = restaurants(:one)
-  end
-
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:restaurants)
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
   end
 
   test "should get new" do
@@ -16,40 +16,25 @@ class RestaurantsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create restaurant" do
-    assert_difference('Restaurant.count') do
-      post :create, restaurant: {  }
-    end
-
-    assert_redirected_to restaurant_path(assigns(:restaurant))
-  end
-
-  test "should show restaurant" do
-    get :show, id: @restaurant
+  test "should get create" do
+    get :create
     assert_response :success
-  end
-
-  test "should raise 404 with an invalid restaurant" do
-    assert_raise(ActiveRecord::RecordNotFound) {
-      get :show, id: 'abcd'
-    }
   end
 
   test "should get edit" do
-    get :edit, id: @restaurant
+    get :edit
     assert_response :success
   end
 
-  test "should update restaurant" do
-    put :update, id: @restaurant, restaurant: {  }
-    assert_redirected_to restaurant_path(assigns(:restaurant))
+  test "should get update" do
+    get :update
+    assert_response :success
   end
 
-  test "should destroy restaurant" do
-    assert_difference('Restaurant.count', -1) do
-      delete :destroy, id: @restaurant
-    end
-
-    assert_redirected_to restaurants_path
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
   end
+
 end
+
