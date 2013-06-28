da866237feee2d4f83b276396e2c618d397c342b
Dan Lourenco <danlourenco@gmail.com>
2013-06-27 22:51:37 -0400

    Add index action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,5 +12,9 @@ class RestaurantsController < ApplicationController
   def show
   	@restaurant = Restaurant.find(params[:id])
   end
+
+  def index
+    @restaurants = Restaurant.all
+  end
   
 end
