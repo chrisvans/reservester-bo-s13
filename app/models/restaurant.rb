108652fcff90b57f1e5de3a9e1e156a0cdb86a59
Nielson Phu <nsnphu@gmail.com>
2013-06-28 16:21:37 -0400

    Added CarrierWave images

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone_number
+  attr_accessible :address, :description, :name, :phone_number, :image
 
   validates :address, :presence => true
   validates :description, :presence => true
   validates :name, :presence => true
   validates :phone_number, :presence => true
+
+  mount_uploader :image, ImageUploader
 end
