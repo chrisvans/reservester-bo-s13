81e1068d3bcb2600a1afd4913c38c1e45ecdbc63
Dan Lourenco <danlourenco@gmail.com>
2013-06-28 15:55:25 -0400

    update restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,10 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone
+  attr_accessible :address, :description, :name, :phone, :image
 
   validates :address, :presence => true
   validates :description, :presence	=> true
   validates :name, :presence => true
   validates :phone, :presence => true,
                     :length => { :minimum => 5 }
-
+  mount_uploader :image, ImageUploader 
 end
