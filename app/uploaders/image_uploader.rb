2bf1744ebde72b93b704477c23fd0655c2f1c30a
Dan Lourenco <danlourenco@gmail.com>
2013-06-29 12:50:19 -0400

    Remove ID from index page, add masthead to show page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
      process :resize_to_limit => [300, 250]
    end
 
+   version :masthead do
+     process :resize_to_limit => [600, 500]
+   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
