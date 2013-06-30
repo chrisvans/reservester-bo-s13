fc429cc1c69bd23700950a658d96271fafa7be24
scit106 <scit106@gmail.com>
2013-06-30 11:49:18 -0500

    added carrierwave gem, image uploader, migration for images

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone
+  attr_accessible :address, :description, :name, :phone, :image
 
   validates :name, presence: true
+
+  mount_uploader :image, ImageUploader
 
 end
---
+++ db/migrate/20130630164626_restaurant_pictures.rb
@@ -0,0 +1,7 @@
+class RestaurantPictures < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
