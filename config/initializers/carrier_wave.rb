40e729ead9563bfb64d0a13126e5626c339dfa7c
Nielson Phu <nsnphu@gmail.com>
2013-07-01 11:38:10 -0400

    Fixed commas bug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
 	config.fog_credentials = {
-		:provider => 'AWS'
-		:aws_access_key_id => "AKIAIE4KP47ZZFDDLXRA" #ENV['AWS_ACCESS_KEY']
+		:provider => 'AWS',
+		:aws_access_key_id => "AKIAIE4KP47ZZFDDLXRA", #ENV['AWS_ACCESS_KEY']
 		:aws_secret_access_key => "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" # ENV['AWS_SECRET_KEY'] 
 
 		# :endpoint => "nsnphu_reservester.s3-website-us-east-1.amazonaws.com"
