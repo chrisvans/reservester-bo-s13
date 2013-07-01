d1b624be59b2f5e9802a15236b40e55651ccf8e6
scit106 <scit106@gmail.com>
2013-07-01 11:55:57 -0400

    added more controller tests. tests currently failing.

--- test/functional/restaurants_controller_test.rb
+++ test/functional/restaurants_controller_test.rb
@@ -6,4 +6,22 @@ class RestaurantsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get new" do
+  	get :new
+  	assert_response :success
+  end
+
+  test "should show restaurant" do
+  	get :show, id: @restaurant
+  	assert_response :success
+  end
+
+  test "should create restaurant" do
+  	assert_difference('Restaurant.count') do
+  		post :create, restaurant: {}
+  	end
+  	assert_redirected_to restaurant_path(assigns(:restaurant))
+  end
+
+
 end
