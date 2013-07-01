14c827600610f1c6a0d8d72a604f0d653a76dccd
Nielson Phu <nsnphu@gmail.com>
2013-07-01 12:11:27 -0400

    Finished S3 integration with security credentials hidden in local ENV variables

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
 	config.fog_credentials = {
 		:provider => 'AWS',
-		:aws_access_key_id => "AKIAIE4KP47ZZFDDLXRA", #ENV['AWS_ACCESS_KEY']
-		:aws_secret_access_key => "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" # ENV['AWS_SECRET_KEY'] 
+		:aws_access_key_id => ENV['AWS_ACCESS_KEY'],
+		:aws_secret_access_key => ENV['AWS_SECRET_KEY']
 
 		# :endpoint => "nsnphu_reservester.s3-website-us-east-1.amazonaws.com"
 	}
