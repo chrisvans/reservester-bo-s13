40c6910c94ee57d75e6cc25529d1490723460fbe
lmbeccaria <luis.m.beccaria@gmail.com>
2013-07-02 08:14:10 -0400

    Bonus2 - Menu uploads

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone, :image, :remote_image_url
+  attr_accessible :address, :description, :name, :phone, :image, :remote_image_url, :menu
 
   mount_uploader :image, ImageUploader
+  mount_uploader :menu, MenuUploader
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { minimum: 6 }
