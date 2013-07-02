b65720fe678b5f18f1e28b28d0f202d4c48611b6
marcomorawec <marco.morawec@gmail.com>
2013-07-02 16:22:31 -0400

    fixing intermittent bug for geocode with after_validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :longitude, :latitude
 
+  after_validation :geocode
+
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,14 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :name, :description, :street, :streetnumber, :city, 
   					:zipcode, :state, :country, :phonenumber, :image, :remote_image_url,
-  					:menue
+  					:menue, :longitude, :latitude
 
 
   mount_uploader :image, ImageUploader
   mount_uploader :menue, MenueUploader
 
 	geocoded_by :zipcode
+	after_validation :geocode
 
 	def address
 	  [street, streetnumber, city, state, zipcode, country].compact.join(', ')
