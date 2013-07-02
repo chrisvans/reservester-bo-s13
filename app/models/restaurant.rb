822cc7b3a8f33512274db6e802fa0b68b9403ab7
Margaret Scott <eudaimonious@gmail.com>
2013-07-02 16:53:43 -0400

    add associations and rake. pull some nav elements right

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ActiveRecord::Base
   attr_accessible :remote_image_url, :image, :city, :country, :description, :phonenumber, :restaurant, :state, :street1, :street2, :zipcode
   validates :restaurant, presence: true
   mount_uploader :image, ImageUploader
+  belongs_to :owner
 end
