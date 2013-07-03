91e2a3c2450c9a550f0f5666276f75cee903340e
Margaret Scott <eudaimonious@gmail.com>
2013-07-02 20:01:35 -0400

    rails g migration add_owner_id_to_restaurant owner_id:integer rake:db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130701193956) do
+ActiveRecord::Schema.define(:version => 20130703000021) do
 
   create_table "owners", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130701193956) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "image"
+    t.integer  "owner_id"
   end
 
 end
