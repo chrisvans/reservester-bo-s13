9cd094da52ef836bb080f3bbd459044baddc6f7a
bpowell123 <bpowell@tulanealumni.net>
2013-07-03 16:40:21 -0400

    Part 1 Login and Part 2 updated models

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'fog', '~> 1.3.1'
 gem 'dotenv-rails'
 gem 'simple_form'
 gem 'rake', '10.1.0'
+gem 'devise'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ActiveRecord::Base
+
+  belongs_to :owner
   attr_accessible :address, :description, :name, :phone, :photo, :photo_cache, :menu
   validates_presence_of :name, :phone
 
