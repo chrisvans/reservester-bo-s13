3b6da771d6c37e3d5312ebe8c70d2dc233d63019
bpowell123 <bpowell@tulanealumni.net>
2013-07-08 11:37:58 -0400

    Finished lab 3.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,5 +9,6 @@ class Owner < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  validates_presence_of :name, :email
   # attr_accessible :title, :body
 end
