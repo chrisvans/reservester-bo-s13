88beab39afaaa1a0e18d672d39460109753cc832
marcomorawec <marco.morawec@gmail.com>
2013-07-10 11:15:15 -0400

    adding the outline for the reservation mailer test

--- test/functional/reservation_mailer_test.rb
+++ test/functional/reservation_mailer_test.rb
@@ -4,4 +4,30 @@ class ReservationMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # testing if the reservation mailer works
+
+  # test "the reservation mailing works" do
+
+  # 	#create a restaurant
+  # 	restaurant = Restaurant.create(:name => "Taco Stand", :owner => "Marco Morawec")
+
+  # 	#creating an owner with an email
+  # 	owner = Owner.create(:name => "Marco Morawec", :email => "marco@gmail.com")
+
+  # 	#assining an owner to the restaurant.
+  # 	owner.restaurants << restaurant
+
+  # 	#create a reservation and 
+  # 	reservation = Reservation.create(:guestname => "John", :guestemail => "marco.morawec@gmail.com", 
+  # 										:reserve_on => "2013-07-15 17:00:06")
+  # 	#assign it ot a restaurant
+  # 	restaurant.reservations << reservation
+
+  # 	#test if the email get's send
+  # 	assert ReservationMailer.reservation_notification(@reservation).deliver
+
+  # end
+
+
 end
