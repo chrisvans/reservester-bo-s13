111dd00a9dad4ed63eecebb4b2c3117866335123
Margaret Scott <eudaimonious@gmail.com>
2013-07-10 12:25:06 -0400

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130703000021) do
+ActiveRecord::Schema.define(:version => 20130710161749) do
 
   create_table "owners", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(:version => 20130703000021) do
 
   add_index "owners", ["email"], :name => "index_owners_on_email", :unique => true
   add_index "owners", ["reset_password_token"], :name => "index_owners_on_reset_password_token", :unique => true
+
+  create_table "reservations", :force => true do |t|
+    t.string   "email"
+    t.datetime "datetime"
+    t.text     "message"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "reservations", ["restaurant_id"], :name => "index_reservations_on_restaurant_id"
 
   create_table "restaurants", :force => true do |t|
     t.string   "restaurant"
