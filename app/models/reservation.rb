858e03b862256d74aad00f42f32bbbb31e5d238e
Margaret Scott <eudaimonious@gmail.com>
2013-07-10 12:29:40 -0400

    Reservations require email and datetime

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   attr_accessible :datetime, :email, :message
+  validates :datetime, presence: true
+  validates :email, presence: true
 end
