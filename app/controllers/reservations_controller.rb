4dc1812c40140742da4675c7b8541a25134a2185
Margaret Scott <eudaimonious@gmail.com>
2013-07-10 12:39:28 -0400

    Require restaurant-owner match to edit update destroy reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,12 @@
 class ReservationsController < ApplicationController
+	before_filter :require_restaurant_owner_match!, :only => [:edit, :update, :destroy]
+
+	def require_restaurant_owner_match!
+    	@restaurant = Restaurant.find(params[:id])
+
+    	unless @restaurant.owner == current_owner
+      		render "unauthorized", :status => :unauthorized
+    	end
+	end
+	
 end
