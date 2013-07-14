da3a4926173fb52c47b68dc917d45707c4d48b35
marcomorawec <marco.morawec@gmail.com>
2013-07-14 16:44:05 -0400

    adding 17 restaurant categories to seed.rb and seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ MyCategories = [
+ 	"American",
+    "Asian, Oriental and Indian",
+    "Barbeque",
+    "Bakeries",
+    "Brew Pubs and Microbreweries",
+    "Coffee Houses",
+    "Family, Homestyle and Steakhouse Dining",
+    "French Restaurants",
+    "German Restaurants",
+    "Greek Restaurants",
+    "Italian Restaurants",
+    "Mediterranean Restaurants",
+    "Mexican Restaurants",
+    "Seafood Restaurants",
+    "Southern Cuisine Restaurants",
+    "Vegetarian Restaurants",
+    "Wine Bars" ]
+
+Categories.delete_all
+
+MyCategories.each do |categories|
+  categories.each_line do |category|
+    name = category.chomp
+    Categories.create!(:name => name)
+  end
+end
+
+
