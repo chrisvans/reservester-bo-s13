b9e5a953cfee9ac8c80e33737a10112b793a4b28
scit106 <scit106@gmail.com>
2013-07-15 13:18:08 -0400

    added categories model

---
+++ db/migrate/20130715160751_create_categories.rb
@@ -0,0 +1,16 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    add_column :restaurants, :category_id, :integer
+
+    create_table :categories_restaurants do |t|
+    	t.belongs_to :restaurant
+    	t.belongs_to :category
+    end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708152935) do
+ActiveRecord::Schema.define(:version => 20130715160751) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "categories_restaurants", :force => true do |t|
+    t.integer "restaurant_id"
+    t.integer "category_id"
+  end
 
   create_table "owners", :force => true do |t|
     t.string   "name",                   :limit => 256
@@ -49,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20130708152935) do
     t.datetime "updated_at",  :null => false
     t.string   "image"
     t.integer  "owner_id"
+    t.integer  "category_id"
   end
 
 end
