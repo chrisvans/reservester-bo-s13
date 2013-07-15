5921233638760e24d29a55d6e16be670e1aa95b3
Dan Lourenco <danlourenco@gmail.com>
2013-07-15 13:52:56 -0400

    Add PDF menu functionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone, :image, :owner
+  attr_accessible :address, :description, :name, :phone, :image, :owner, :menu
 
   #validates :address, :presence => true
   #validates :description, :presence	=> true
@@ -8,6 +8,7 @@ class Restaurant < ActiveRecord::Base
   #validates :phone, :phone_number => {:format => /\d{3}-\d{3}-\d{4}/}
   
   mount_uploader :image, ImageUploader 
+  mount_uploader :menu, MenuUploader
   
   has_many :reservations
   belongs_to :owner
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710162633) do
+ActiveRecord::Schema.define(:version => 20130715164352) do
 
   create_table "owners", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20130710162633) do
     t.datetime "updated_at",  :null => false
     t.string   "image"
     t.integer  "owner_id"
+    t.string   "menu"
   end
 
 end
