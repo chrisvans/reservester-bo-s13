42afca67924944c937138f77cc149b086cefc716
Nielson Phu <nsnphu@gmail.com>
2013-07-17 12:09:15 -0400

    Cleaned up some joins using a helper

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,5 @@
 module RestaurantsHelper
+	def category_link_list(categories)
+		categories.map { |cat| link_to "#{cat.titleized_name} (#{cat.restaurants.count})", category_path(cat) }.join("---").html_safe
+  	end
 end
