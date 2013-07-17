32e8827e1eab2f45e0029d7cfa350faa74bbb20b
scit106 <scit106@gmail.com>
2013-07-17 16:04:19 -0400

    added owner? and patron? methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
 	def dashboard
 		@restuarants = current_user.restaurants
 	end
+
+	def owner?
+		if current_user.role == 'owner'
+			then true
+		end
+	end
+
+	def patron?
+		if current_user.role == 'patron'
+			then true
+		end
+	end
 end
