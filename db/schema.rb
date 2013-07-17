572aad41a65efd8db1e4108e696c3295a2286e00
scit106 <scit106@gmail.com>
2013-07-17 15:57:35 -0400

    added role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717162614) do
+ActiveRecord::Schema.define(:version => 20130717194929) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(:version => 20130717162614) do
 
   create_table "users", :force => true do |t|
     t.string   "name",                   :limit => 256
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",                    :default => "", :null => false
+    t.string   "email",                                 :default => "",       :null => false
+    t.string   "encrypted_password",                    :default => "",       :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130717162614) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "role",                                  :default => "patron"
   end
 
   add_index "users", ["email"], :name => "index_owners_on_email", :unique => true
