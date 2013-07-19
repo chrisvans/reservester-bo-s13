08887fcac3450ba1516500413503294a04dbb20f
Jean-Charles Sisk <jasisk@gmail.com>
2013-07-19 12:00:48 -0400

    adding json response to restaurants#index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,10 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @restaurants }
+    end
   end
 
   def show
