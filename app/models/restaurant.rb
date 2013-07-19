a39efdffffe8574751dc34a5c21ecd041bf7d4f3
chrisvans <chrisvanschyndel@gmail.com>
2013-07-19 11:41:02 -0400

    Changed structure slightly, improved look of search, added in search link to main page.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :category_ids, :address, :description, :name, :phone, :photo, :photo_cache, :pdf, :longitude, :latitude, :restaurant_id
+  # attr_accessor :category_tokens
   mount_uploader :photo, PhotoUploader
   mount_uploader :pdf, PdfUploader
 
@@ -13,7 +14,7 @@ class Restaurant < ActiveRecord::Base
   accepts_nested_attributes_for :reservations, :reject_if => lambda { |a| a[:email].blank? }, :allow_destroy => true
   validates_presence_of :name
 
-  def category_tokens=(ids)
-    self.category_ids = ids.split(",")
-  end
+  # def category_tokens=(ids)
+  #   self.category_ids = ids.split(",")
+  # end
 end
