6f3ad340a039fd8e9f77610b7933e16fb161c433
scit106 <scit106@gmail.com>
2013-07-19 12:12:45 -0400

    fixing owners migration to not include emails so forward migrations work on heroku

--- db/migrate/20130701162523_create_owners.rb
+++ db/migrate/20130701162523_create_owners.rb
@@ -2,7 +2,7 @@ class CreateOwners < ActiveRecord::Migration
   def change
     create_table :owners do |t|
     	t.string :name
-    	t.string :email
+    	# t.string :email
       	t.timestamps
     end
   end
