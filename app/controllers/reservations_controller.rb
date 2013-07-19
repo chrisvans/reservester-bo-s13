107a38dd81653df64aa1407ff65dc87d33fe0d37
Margaret Scott <eudaimonious@gmail.com>
2013-07-19 14:56:54 -0400

    Work on mailer

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,11 +8,14 @@ class ReservationsController < ApplicationController
 		@reservation = current_restaurant.reservations.build(params[:reservation])
 		respond_to do |format|
 			if @reservation.save
+				#Send a reservation notification email to the restaurant's owner
+				puts "beginning of action response"
 				ReservationMailer.reservation_notification(@reservation).deliver
     			format.html  { redirect_to(current_restaurant,
     							:notice => 'Your reservation has been accepted. See you soon!') }
     			format.json  { render :json => @reservation,
     							:status => :created, :location => @reservation }
+    			puts "end of action response"
       		else
         		format.html { render 'restaurant/show' }
         		format.json { render :json => current_restaurant.errors,
--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,7 +2,9 @@ class ReservationMailer < ActionMailer::Base
   	default from: "margaret@scott.com"
 
 	def reservation_notification(reservation)
+		puts "hello I am the notification beginning"
 		owner = reservation.restaurant.owner
 		mail(:to => "#{owner.name} <#{owner.email}>", :subject => "Reservation Created")
+		puts "hello I am the notification ending"
 	end
 end
