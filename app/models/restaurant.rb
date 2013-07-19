fc93adb95c73e718d0b62f88b2e83cd769dc4931
David Zabner <david.zabner@gmail.com>
2013-07-19 12:42:27 -0400

    latest changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
 
 	belongs_to :owner
-	has_many :reservations, dependent :destroy
+	has_many :reservations
 
   attr_accessible :address, :description, :name, :phone_number, :image, :remote_image_url, :id, :owner_id
   validates :name, presence: true, length: { minimum: 2}
