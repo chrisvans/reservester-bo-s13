3da96f5a38dd9e0c0bd4d3240c7ae1d75e64b04b
Dan Lourenco <danlourenco@gmail.com>
2013-07-19 12:30:20 -0400

    prevent initializing application on precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Reservester
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added as troubleshooting step performed after reading this article https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
