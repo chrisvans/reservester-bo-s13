f3fdc7f70c17104629410d17f9b4f49f06b57622
Dan Lourenco <danlourenco@gmail.com>
2013-07-21 15:33:28 -0400

    fix table issue

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,7 +44,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    end
 
    version :masthead do
-     process :resize_to_limit => [600, 500]
+     process :resize_to_limit => [195, 210]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
